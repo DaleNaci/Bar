4df19cfd357fd3493b28582f952adf38b0342d04
DaleNaci <dale.nash01@gmail.com>
2018-07-20 21:41:05 -0400

    Initialized file and started intepret function

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main
+{
+
+	private static int ptr;
+	private static byte memory[] = new byte[65536];
+
+	private static void interpret(String s)
+	{
+		int counter = 0;
+		String[] split = s.split("|");
+		int[] commands = new int[split.length];
+	}
+
+	public static void main(String[] args)
+	{
+
+
+
+	}
+
+}
