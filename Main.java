6393ec2248590c700665413c1764e39056532428
DaleNaci <dale.nash01@gmail.com>
2018-07-20 23:57:45 -0400

    Added functionality for 5/7 functions, still working on loops

--- Main.java
+++ Main.java
@@ -8,15 +8,68 @@ class Main
 
 	private static void interpret(String s)
 	{
-		int counter = 0;
-		String[] split = s.split("|");
-		int[] commands = new int[split.length];
+		int c = 0;
+		ptr = 0;
+		String[] split = s.split("\\|");
+		// for (int i=0;i<split.length;i++) {
+		// 	System.out.println(split[i]);
+		// }
+		for (int i=0;i<split.length;i++) {
+			switch(split[i]) {
+				case "":  memory[ptr]++; break; // Works
+				case " ": memory[ptr]--; break; // Works
+				case "  ": // Works
+					if (ptr == memory.length - 1)
+						ptr = 0;
+					else
+						ptr++;
+					break;
+				case "   ": // Works
+					if (ptr == 0)
+						ptr = memory.length - 1;
+					else
+						ptr--;
+					break;
+				case "    ": // Works (MODIFIED)
+					System.out.print((char)(memory[ptr]+65)); break;
+				case "     ":
+					if (memory[ptr] == 0)
+					{
+						i++;
+						while (c > 0 || split[i].equals("      "))
+						{
+							if (split[i].equals("     "))
+								c++;
+							else if (split[i].equals("      "))
+								c--;
+							i++;
+						}
+					}
+					break;
+				case "      ":
+					if (memory[ptr] != 0)
+					{
+						i--;
+						while (c > 0 || split[i].equals("     "))
+						{
+							if (split[i].equals("      "))
+								c++;
+							else if (split[i].equals("     "))
+								c--;
+							i--;
+						}
+						i--;
+					}
+					break;
+			}
+		}
 	}
 
 	public static void main(String[] args)
 	{
 
-
+		interpret("|  |||     |");
+		System.out.println();
 
 	}
 
